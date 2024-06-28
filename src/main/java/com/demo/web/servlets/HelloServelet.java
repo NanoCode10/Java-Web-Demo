9a5b463f99ac811133c5e09c5d0041877c36810a
NanoCode10 <marianoars10@gmail.com>
2024-06-28 14:22:23 -0300

    first commit

---
+++ src/main/java/com/demo/web/servlets/HelloServelet.java
@@ -0,0 +1,21 @@
+package com.demo.web.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServelet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World! By NanoCode10 🙌</h1>");
+    }
+    
+}
